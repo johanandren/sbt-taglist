6113a8e6e89d7e64ecb34881e4154e25ba0831a5
Johan Andrén <johan@markatta.com>
2017-08-18 15:54:18 +0200

    Support for sbt 1.0.0, crossbuilding and scripted tests

---
+++ scripted.sbt
@@ -0,0 +1,4 @@
+scriptedLaunchOpts := { scriptedLaunchOpts.value ++
+  Seq("-Xmx1024M", "-XX:MaxPermSize=256M", "-Dplugin.version=" + version.value)
+}
+scriptedBufferLog := false
--- src/main/scala/TagListPlugin.scala
+++ src/main/scala/TagListPlugin.scala
@@ -1,11 +1,14 @@
 package com.markatta.sbttaglist
 
 import scala.io.{Codec, Source}
-import sbt._
+import sbt.{Def, _}
 import Keys._
 
-object TagListPlugin extends Plugin {
+object TagListPlugin extends AutoPlugin {
 
+
+  override def trigger = allRequirements
+  override def requires = empty
 
   sealed trait LogLevel {
     def log(log: Logger, message: String)
@@ -21,72 +24,45 @@ object TagListPlugin extends Plugin {
 
   case class Tag(word: Word, level: LogLevel = Warn)
 
-  object TagListKeys {
-    val tagWords = SettingKey[Set[String]]("tag-list-words", "Tag words to look for when searching for tagged files")
-    val tags = SettingKey[Set[Tag]]("tag-list-tags", "Detailed tags with a log level per tag word")
-    val skipChars = SettingKey[Set[Char]]("tag-list-skip-chars", "Characters to skip around tag words")
-    val tagList = TaskKey[TagList]("tag-list", "Display all TODO tags in the sources of the project")
-    val tagsSourceEncoding = SettingKey[Codec]("tags-source-encoding", "The encoding of the sources")
+  object autoImport {
+    lazy val tagListWords = SettingKey[Set[String]]("tag-list-words", "Tag words to look for when searching for tagged files")
+    lazy val tags = SettingKey[Set[Tag]]("tag-list-tags", "Detailed tags with a log level per tag word")
+    lazy val tagListSkipChars = SettingKey[Set[Char]]("tag-list-skip-chars", "Characters to skip around tag words")
+    lazy val tagList = TaskKey[TagList]("tag-list", "Display all TODO tags in the sources of the project")
   }
 
-  import TagListKeys._
+  import autoImport._
 
-  lazy val tagListSettings = Seq(
-    tagListTask,
-    tagWords := Set("todo", "fixme"),
-    tags <<= tagWords(_.map(word => Tag(word, Warn))),
-    skipChars := Set('/', ':'),
-    tagsSourceEncoding := Codec("UTF-8")
-  )
+  override lazy val projectSettings = Seq(
+    tagListWords := Set("todo", "fixme"),
+    tags := tagListWords.value.map(word => Tag(word, Warn)),
+    tagListSkipChars := Set('/', ':'),
+    tagList := {
+      val srcs = (sources in Compile).value
+      val tagWords = tags.value
+      val s = streams.value
+      val skip = tagListSkipChars.value
 
-  lazy val tagListTask = tagList <<= (sources in Compile, tags, streams, skipChars, tagsSourceEncoding) map {
-    (sources: Seq[File], tagWords: Set[Tag], streams: TaskStreams, skipChars: Set[Char], encoding: Codec) => {
-      val tagList = generateTagList(sources, tagWords, skipChars, encoding)
+
+      val tagList = TagListGenerator.generateTagList(srcs, tagWords, skip, Codec.UTF8)
 
       val count = tagList.map(_._2.size).sum
       if (count > 0) {
-        streams.log.warn("Tags found: %s" format count)
+        s.log.warn("Tags found: %s" format count)
       }
 
       for {
         (file, tags) <- tagList
         (lineNumber, tagLine, logLevel) <- tags
       } {
-        logLevel.log(streams.log, file.getName + ":" + lineNumber + ": " + tagLine.trim)
+        logLevel.log(s.log, file.getName + ":" + lineNumber + ": " + tagLine.trim)
       }
 
       tagList
     }
-
-  }
-
- private def generateTagList(files: Seq[File], tags: Set[Tag], skipChars: Set[Char], sourceEncoding: Codec): TagList = {
-
-    val trie = Trie(tags.map(_.word.toLowerCase))
-    val logLevelPerWord: Map[Word, LogLevel] = tags.map(t => t.word.toLowerCase -> t.level).toMap
-
-    files.par.map { file =>
-
-      val linesWithIndexes = Source.fromFile(file)(sourceEncoding).getLines().zipWithIndex
-
-      val matchesInFile = linesWithIndexes.foldLeft(Seq[(LineNumber, Line, LogLevel)]()) { case (acc, (line, lineNumber)) =>
-
-        val foundWordsInLine = line.toLowerCase.split(' ').foldLeft(Seq[Word]()) { (wordAcc, word) =>
-          val cleanWord = TextUtils.dropFromBothEnds(word.toList, skipChars).mkString
-          if (trie.contains(cleanWord))
-            wordAcc :+ cleanWord
-          else
-            wordAcc
-        }
+  )
 
 
-        acc ++ foundWordsInLine.map(word => (lineNumber, line, logLevelPerWord(word)))
-      }
-
-      file -> matchesInFile
-    }.seq
-
-  }
 
 
 }
