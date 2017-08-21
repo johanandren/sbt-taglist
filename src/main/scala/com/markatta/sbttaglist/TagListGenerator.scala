12913e075b4fd1cc1a839d50b66c3ffd230aab51
Johan Andrén <johan@markatta.com>
2017-08-21 14:44:45 +0200

    Use arraybuilder

--- src/main/scala/com/markatta/sbttaglist/TagListGenerator.scala
+++ src/main/scala/com/markatta/sbttaglist/TagListGenerator.scala
@@ -3,19 +3,17 @@ package com.markatta.sbttaglist
 import com.markatta.sbttaglist.TagListPlugin._
 import sbt.File
 
-import scala.collection.mutable
 import scala.io.{Codec, Source}
 
 object TagListGenerator {
   def generateTagList(files: Seq[File], tags: Set[Tag], skipChars: Set[Char], sourceEncoding: Codec): TagList = {
-
     val trie = Trie(tags.map(_.word.toLowerCase))
     val logLevelPerWord: Map[Word, LogLevel] = tags.map(t => t.word.toLowerCase -> t.level).toMap
 
     files.par.map { file =>
       var lineNumber = 0
       val lines = Source.fromFile(file)(sourceEncoding).getLines()
-      val matchesInFile = mutable.Buffer.empty[(LineNumber, Line, LogLevel)]
+      val matchesInFile = Array.newBuilder[(LineNumber, Line, LogLevel)]
 
       while (lines.hasNext) {
         lineNumber += 1
@@ -24,12 +22,12 @@ object TagListGenerator {
         while(wordIterator.hasNext) {
           val cleanWord = TextUtils.dropFromBothEnds(wordIterator.next(), skipChars)
           if (trie.contains(cleanWord.iterator)) {
-            matchesInFile.append((lineNumber, line, logLevelPerWord(cleanWord.mkString)))
+            matchesInFile += ((lineNumber, line, logLevelPerWord(cleanWord.mkString)))
           }
         }
       }
 
-      file -> matchesInFile
+      file -> matchesInFile.result().toSeq
     }.seq
   }
 }
